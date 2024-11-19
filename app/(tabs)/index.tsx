119d94517e027965c5811a0b4edd5d5f00c3c150
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-11-20 00:15:11 +0800

    feat: Add calendar to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,40 @@
 import { View } from "react-native";
 import { Text } from "~/components/ui/text";
+import {
+  Calendar,
+  CalendarTheme,
+  toDateId,
+} from "@marceloterreiro/flash-calendar";
+import { useState } from "react";
+
+const theme: CalendarTheme = {
+  rowMonth: {
+    content: {
+      fontWeight: 700,
+    },
+  },
+  itemWeekName: {
+    content: {
+      fontWeight: 600,
+      fontSize: 16,
+    },
+  },
+};
+
 export default function Screen() {
+  const [selectedDate, setSelectedDate] = useState(toDateId(new Date()));
+  const handleCalendarDayPress = (day: string) => {
+    console.log(day);
+    setSelectedDate(day);
+  };
+
   return (
-    <View className="flex flex-col flex-1">
-      <Text>Physio Tracker</Text>
+    <View className="flex flex-col flex-1 p-8 gap-4">
+      <Calendar
+        calendarMonthId={selectedDate}
+        onCalendarDayPress={handleCalendarDayPress}
+        theme={theme}
+      />
     </View>
   );
 }
