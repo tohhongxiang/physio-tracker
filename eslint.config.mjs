3d9a5d97749ccfd5a47dc099421d1855d6d8de17
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-12-09 23:45:58 +0800

    feat: Add eslint-plugin-react-hooks and lint command

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import gitignore from "eslint-config-flat-gitignore";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	gitignore(),
 	{
 		ignores: ["tailwind.config.js"]
 	},
@@ -15,6 +18,12 @@ export default [
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 	pluginReact.configs.flat.recommended,
+	{
+		plugins: {
+			"react-hooks": eslintPluginReactHooks
+		},
+		rules: eslintPluginReactHooks.configs.recommended.rules
+	},
 	{
 		ignores: ["node_modules/*"],
 		rules: {
