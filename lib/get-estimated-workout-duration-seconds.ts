50f43d7b9cf8531a5d2af2aebd07b17346dbae51
Toh Hong Xiang <tohhongxiang@gmail.com>
2025-01-12 18:27:40 +0800

    fix: Minimum estimated workout duration should be one minute

--- lib/get-estimated-workout-duration-seconds.ts
+++ lib/get-estimated-workout-duration-seconds.ts
@@ -1,10 +1,12 @@
 import { Workout } from "~/types";
 
+const MINIMUM_ESTIMATED_DURATION_SECONDS = 60;
+
 const ESTIMATED_REP_DURATION_SECONDS = 1;
 const ESTIMATED_REST_BETWEEN_SETS_SECONDS = 15;
 
 export default function getEstimatedWorkoutDurationSeconds(workout: Workout) {
-	let rawEstimatedDuration = 0;
+	let rawEstimatedDurationSeconds = 0;
 
 	workout.exercises.forEach((exercise) => {
 		const durationForReps =
@@ -20,13 +22,13 @@ export default function getEstimatedWorkoutDurationSeconds(workout: Workout) {
 				ESTIMATED_REST_BETWEEN_SETS_SECONDS) *
 			(exercise.sets - 1);
 
-		rawEstimatedDuration +=
+		rawEstimatedDurationSeconds +=
 			durationForReps +
 			durationForRestBetweenReps +
 			durationForRestBetweenSets;
 	});
 
-	const estimatedDuration = Math.ceil(rawEstimatedDuration / 60) * 60;
+	const estimatedDuration = Math.ceil(rawEstimatedDurationSeconds / 60) * 60; // round up to nearest minute
 
-	return estimatedDuration;
+	return Math.max(MINIMUM_ESTIMATED_DURATION_SECONDS, estimatedDuration); // make sure minimum estimated duration is 1 minute
 }
