158e3dda8ec850947cfcfd732949ee100d6de5fa
Toh Hong Xiang <tohhongxiang@gmail.com>
2025-06-22 11:25:58 +0800

    fix: Improve estimated duration for workout

--- lib/get-estimated-workout-duration-seconds.ts
+++ lib/get-estimated-workout-duration-seconds.ts
@@ -2,24 +2,30 @@ import { Workout } from "~/types";
 
 const MINIMUM_ESTIMATED_DURATION_SECONDS = 60;
 
-const ESTIMATED_REP_DURATION_SECONDS = 1;
-const ESTIMATED_REST_BETWEEN_SETS_SECONDS = 15;
+const ESTIMATED_REP_DURATION_SECONDS = 2;
+const ESTIMATED_REST_BETWEEN_SETS_SECONDS = 30;
+const ESTIMATED_REST_BETWEEN_EXERCISES_SECONDS = 30;
 
 export default function getEstimatedWorkoutDurationSeconds(workout: Workout) {
 	let rawEstimatedDurationSeconds = 0;
 
 	workout.exercises.forEach((exercise) => {
 		const durationForReps =
-			(exercise.durationPerRepSeconds ?? ESTIMATED_REP_DURATION_SECONDS) *
+			(exercise.durationPerRepSeconds > 0
+				? exercise.durationPerRepSeconds
+				: ESTIMATED_REP_DURATION_SECONDS) *
 			exercise.reps *
 			exercise.sets;
+
 		const durationForRestBetweenReps =
 			exercise.restBetweenRepsSeconds *
 			(exercise.reps - 1) *
 			exercise.sets;
+
 		const durationForRestBetweenSets =
-			(exercise.restBetweenSetsSeconds ??
-				ESTIMATED_REST_BETWEEN_SETS_SECONDS) *
+			(exercise.restBetweenSetsSeconds > 0
+				? exercise.restBetweenRepsSeconds
+				: ESTIMATED_REST_BETWEEN_SETS_SECONDS) *
 			(exercise.sets - 1);
 
 		rawEstimatedDurationSeconds +=
@@ -28,7 +34,16 @@ export default function getEstimatedWorkoutDurationSeconds(workout: Workout) {
 			durationForRestBetweenSets;
 	});
 
-	const estimatedDuration = Math.ceil(rawEstimatedDurationSeconds / 60) * 60; // round up to nearest minute
+	const durationForRestBetweenExercises =
+		(workout.exercises.length - 1) *
+		ESTIMATED_REST_BETWEEN_EXERCISES_SECONDS;
+	rawEstimatedDurationSeconds += durationForRestBetweenExercises;
 
-	return Math.max(MINIMUM_ESTIMATED_DURATION_SECONDS, estimatedDuration); // make sure minimum estimated duration is 1 minute
+	const estimatedDurationMinutes =
+		Math.ceil(rawEstimatedDurationSeconds / 60) * 60; // round up to nearest minute
+
+	return Math.max(
+		MINIMUM_ESTIMATED_DURATION_SECONDS,
+		estimatedDurationMinutes
+	); // make sure minimum estimated duration is 1 minute
 }
