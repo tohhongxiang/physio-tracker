52b954e02cbd46f6a316654f3009e2fff8910de0
Toh Hong Xiang <tohhongxiang@gmail.com>
2025-09-17 21:44:11 +0800

    feat: Migrate from expo-av to expo-audio

--- app.config.js
+++ app.config.js
@@ -35,7 +35,7 @@ export default {
 		output: "static",
 		favicon: "./assets/images/favicon.png"
 	},
-	plugins: ["expo-router", "expo-av", "expo-sqlite"],
+	plugins: ["expo-router", "expo-sqlite", "expo-audio"],
 	experiments: {
 		typedRoutes: true
 	},
--- hooks/use-sound.ts
+++ hooks/use-sound.ts
@@ -1,20 +1,12 @@
-import { AVPlaybackSource, Audio } from "expo-av";
-import { useCallback, useEffect, useMemo } from "react";
+import { AudioSource, useAudioPlayer } from "expo-audio";
+import { useCallback } from "react";
 
-export default function useSound(audioSource: AVPlaybackSource) {
-	const player = useMemo(() => new Audio.Sound(), []);
+export default function useSound(audioSource: AudioSource) {
+	const player = useAudioPlayer(audioSource);
 
-	useEffect(() => {
-		player.loadAsync(audioSource);
-
-		return () => {
-			player.unloadAsync();
-		};
-	}, [audioSource, player]);
-
-	const play = useCallback(async () => {
-		await player.setPositionAsync(0);
-		await player.playAsync();
+	const play = useCallback(() => {
+		player.seekTo(0);
+		player.play();
 	}, [player]);
 
 	return { play };
